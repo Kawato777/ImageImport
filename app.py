43e7e4efd3c0937fe0a60fd9a8e81ceeeeb8f66f
Kawato <keiaru0512@gmail.com>
2022-03-14 22:17:16 +0900

    画像表示完了

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask import render_template, request, redirect
 from flask_bootstrap import Bootstrap
+import datetime
 import re
+
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,10 +18,23 @@ def index():
         # URLチェック
         pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
         if re.match(pattern, url):
-            print("Is URL:" + url)
-            return render_template("finding.html",url=url)
+            # 時間取得
+            t_delta = datetime.timedelta(hours=9)
+            JST = datetime.timezone(t_delta, 'JST')
+            now = datetime.datetime.now(JST)
+            time = now.strftime("%Y%m%d")
+
+            # 画像取得
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text,'lxml')
+            allImages = soup.findAll("img")
+            images = []
+            for image in allImages:
+                if(image["src"].startswith("http")):
+                    images.append(image["src"])
+
+            return render_template("result.html",url=url ,images = images ,imgLen = len(images))
         else:  
-            print("Not URL:" + url)
-            return redirect("/")
+            return render_template("error.html",errorText="正しいURLを入力してください。")
     else:
         return render_template("index.html")
