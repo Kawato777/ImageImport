174281438b38eb642df1cf4dbf1ba8983ab1f091
Kawato <keiaru0512@gmail.com>
2022-03-14 16:59:53 +0900

    html共通化_1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template, request, redirect
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-    return "Hello World!!"
+    return render_template("index.html")
